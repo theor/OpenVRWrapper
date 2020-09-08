af7ca226e613a7e51716650548d22614722f43ba
Theo Richart <theo.richart@gmail.com>
2020-09-08 09:58:45 -0400

    Init

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Valve.VR;
+
+namespace OpenVRWrapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            EVRInitError error = EVRInitError.None;
+            var sys = OpenVR.Init(ref error);
+            // var i = OpenVRInterop.InitInternal(ref error, EVRApplicationType.VRApplication_Utility);
+            try
+            {
+                Console.WriteLine($"Error: {error}");
+                Console.WriteLine($"PResent: {sys.IsTrackedDeviceConnected(0)}");
+                Console.WriteLine($"PResent: {OpenVRInterop.IsHmdPresent()}");
+
+            }
+            finally
+            {
+                OpenVR.Shutdown();
+                // OpenVRInterop.ShutdownInternal();
+                
+            }
+        }
+    }
+}
